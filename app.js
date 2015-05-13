c52a2d72b7c756244c53a022f40e5e6f45c9bbb3
Nitin Pradeep Kumar <nkumar@appcelerator.com>
2015-05-13 19:06:14 +0800

    verify and get required dataset from github

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
-function main(){
+var queryEndpoint = 'https://api.github.com/legacy/repos/search/';
 
+function main() {
+
+  $("#submit").click(function() {
+    var query = $("#search").val();
+    console.log(query)
+    getRepoData(query);
+  });
+  
+}
+
+function getRepoData(query) {
+
+  $.get(queryEndpoint + query, function(data, status) {
+    var result = [];
+    if (status == 'success') {
+      var raw = data.repositories;
+      for (var i = 0; i < raw.length; i++) {
+        var temp = {};
+        temp.name = raw[i].name;
+        temp.owner = raw[i].owner;
+        temp.owner_name = raw[i].owner + raw[i].name;
+        temp.followers = raw[i].followers;
+        temp.url = raw[i].url;
+        temp.description = raw[i].description;
+        temp.language = raw[i].language;
+        result.push(temp);
+      }
+
+      console.log(result);
+
+    } else {
+      console.log('Failed to get data from Github.');
+    }
+
+  });
 }
 
 $(document).ready(main);
